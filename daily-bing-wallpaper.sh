1f7be6a563dc056eefcdea3de07f581c7e3109b5
lyladuan <lyladuan@gmail.com>
2016-03-28 21:09:06 -0700

    adding new function checkConnection

--- daily-bing-wallpaper.sh
+++ daily-bing-wallpaper.sh
@@ -6,35 +6,69 @@
 # do shell script "/usr/local/sbin/sleepwatcher -w /path/to/wake.sh &> /dev/null &"
 
 # Define variables 
-FileLocation="/Users/{yourUserName}/Pictures/Bing"
+FileLocation="/Users/jduan/Pictures/Bing"
 cd $FileLocation
 
 Date=`date +%Y-%m-%d`
 
 # define download wallpaper function 
 function getWallpaper() {
-	wget --output-document=bing.html bing.com
+	/usr/local/bin/wget --output-document=bing.html bing.com
 	rm ./$Date.jpg
-	wget --output-document=$Date.jpg `sed -n "s/^.*g_img *= *{ *url:'\([^']*\)'.*$/\1/p" < bing.html | sed 's/^&quot;\(.*\)&quot;$/\1/' | sed 's/^\/\(.*\)/http:\/\/www.bing.com\/\1/'`
+	/usr/local/bin/wget --output-document=$Date.jpg `sed -n "s/^.*g_img *= *{ *url:'\([^']*\)'.*$/\1/p" < bing.html | sed 's/^&quot;\(.*\)&quot;$/\1/' | sed 's/^\/\(.*\)/http:\/\/www.bing.com\/\1/'`
 	rm ./bing.html
 }
 
 # define set wallpaper function 
 function setWallpaper() {
 	# mac os
-	sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = FileLocation + '/$Date.jpg'";
+	sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '$FileLocation/$Date.jpg'";
 	killall Dock
 }
 
-# check if file exist
-if [ -f FileLocation + $Date.jpg ]; then
-    # if exist exit
-	echo "File exists, exiting script"
-	exit
-else 
-	echo "Downloading new wallpaper"
-	# get the wallpaper and execute
-	getWallpaper
-	setWallpaper
-fi
+# define ping bing.com function 
+
+function checkConnection() {
+
+	((count = 200))                            # Set maximum number to try.
+	while [[ $count -ne 0 ]] ; do
+	    ping -c 1 www.bing.com                 # Try once.
+	    successFlag=$?                          # if successFlag = 0 then success if 1 then unsuccess 
+	    if [[ $successFlag -eq 0 ]] ; then
+	        ((count = 1))                      # If okay, flag to exit loop.
+	    fi
+	    ((count = count - 1))                  # So we don't go forever.
+	done
+
+	return $successFlag 					   #return variable 
+}
+
+# sleep 60                                     # sunsetting this sleeping method 
+
+# check if the connection is success 
+
+if [[ $successFlag -eq 0 ]] ; then                  # Make final determination.
+    echo "The connection is successful."
+
+    # if successful then do the following
+	# check if file exist
+	if [ -f $FileLocation/$Date.jpg ]; then
+	    # if exist exit
+		echo "File exists, exiting script"
+		exit
+	else 
+		echo "Downloading new wallpaper"
+		# get the wallpaper and execute
+		getWallpaper
+		setWallpaper
+	fi
+
+	# else not successful then standard output the following
+else
+    echo "Your internet connection is not established."
+fi	
+
+
+
+
 
